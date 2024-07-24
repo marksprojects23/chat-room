35fbe4a84471fb68a5d7f593689168614c66e037
cui19001 <markc7856@yahoo.com>
2024-07-23 18:56:36 -0700

    getting less desperate

--- src/chat_client.erl
+++ src/chat_client.erl
@@ -1,20 +1,28 @@
 -module(chat_client).
--export([start_client/2, client/1]).
+-export([start_client/2, client_loop/1, send_message/2]).
 
 start_client(IP, Port) ->
-    {ok, Socket} = gen_tcp:connect(IP, Port, [binary, {packet, 0}, {active, true}]),
-    spawn(fun() -> client(Socket) end),
-    Socket.
+    io:format("Attempting to connect to ~s:~p~n", [IP, Port]),
+    case gen_tcp:connect(IP, Port, [binary, {packet, 0}, {active, once}]) of
+        {ok, Socket} ->
+            io:format("Connected to server, socket: ~p~n", [Socket]),
+            spawn(fun() -> client_loop(Socket) end),
+            Socket;
+        {error, Reason} ->
+            io:format("Failed to connect: ~p~n", [Reason])
+    end.
 
-client(Socket) ->
-    io:format("Waiting to receive data on socket: ~p~n", [Socket]),
+client_loop(Socket) ->
     receive
         {tcp, Socket, Message} ->
             io:format("Received from server: ~p~n", [Message]),
             inet:setopts(Socket, [{active, once}]),
-            client(Socket);
-        {tcp_closed, _} ->
+            client_loop(Socket);
+        {tcp_closed, Socket} ->
             io:format("Connection closed~n");
         {tcp_error, Socket, Reason} ->
             io:format("Connection error: ~p~n", [Reason])
     end.
+
+send_message(Socket, Message) ->
+    gen_tcp:send(Socket, Message).
--- src/chat_server.erl
+++ src/chat_server.erl
@@ -1,5 +1,5 @@
 -module(chat_server).
--export([start/0, accept_connections/1, handle_client/1]).
+-export([start/0, accept_connections/1, handle_client/2]).
 
 -define(TCP_OPTIONS, [{active, false}, {packet, 0}, {reuseaddr, true}]).
 
@@ -11,24 +11,42 @@ start() ->
 accept_connections(ListenSocket) ->
     {ok, ClientSocket} = gen_tcp:accept(ListenSocket),
     io:format("Client connected: ~p~n", [ClientSocket]),
-    spawn(fun() -> handle_client(ClientSocket) end),
-    gen_tcp:send(ClientSocket, <<"Welcome to the chat server!">>),
+    inet:setopts(ClientSocket, [{active, once}]),
+    spawn(fun() -> handle_client(ClientSocket, self()) end),
     accept_connections(ListenSocket).
 
-handle_client(Socket) ->
-    io:format("Setting socket to active once: ~p~n", [Socket]),
-    inet:setopts(Socket, [{active, once}]),
+handle_client(Socket, ServerPid) ->
     receive
         {tcp, Socket, Data} ->
             io:format("Received data: ~p~n", [Data]),
-            Result = gen_tcp:send(Socket, Data),
-            io:format("Sent data result: ~p~n", [Result]),
+            ServerPid ! {broadcast, Socket, Data},
             inet:setopts(Socket, [{active, once}]),
-            handle_client(Socket);
+            handle_client(Socket, ServerPid);
         {tcp_closed, Socket} ->
             io:format("Client disconnected: ~p~n", [Socket]),
-            ok;
+            ServerPid ! {remove_client, Socket};
         {tcp_error, Socket, Reason} ->
             io:format("Connection error: ~p~n", [Reason]),
-            ok
+            ServerPid ! {remove_client, Socket}
+    end.
+
+init() ->
+    register(chat_server, self()),
+    loop([]).
+
+loop(Clients) ->
+    receive
+        {broadcast, From, Message} ->
+            lists:foreach(fun(Client) ->
+                if Client =/= From ->
+                    gen_tcp:send(Client, Message);
+                true -> ok
+                end
+            end, Clients),
+            loop(Clients);
+        {remove_client, Socket} ->
+            NewClients = lists:delete(Socket, Clients),
+            loop(NewClients);
+        {new_client, Socket} ->
+            loop([Socket | Clients])
     end.
